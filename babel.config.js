8f19c77614f213f00b4746f036ce4bf79ec784c8
Fernandam99 <mm43651@gmail.com>
2025-11-25 23:43:53 -0500

    chore(config): corregi la configuración de Babel

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [['babel-preset-expo', { jsxPragma: 'React' }], 'nativewind/babel'],
+    plugins: [
+      ['@babel/plugin-transform-runtime', {
+        helpers: true,
+        regenerator: true,
+      }]
+    ],
   };
 };
